cd73fbb1de2123b20bc4d0593e05747c8421f7b2
nateBannisZ <bannis52@gmail.com>
2020-07-26 12:44:36 -0400

    added the ability to communicate the form to the database fixed a bug where the form would not submit

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,23 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import {NgForm} from '@angular/forms';
+import {AuthService} from './auth.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
-export class AuthComponent implements OnInit {
+export class AuthComponent{
   isLoginMode = true;
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   onSwitchMode(){
     this.isLoginMode = !this.isLoginMode;
   }
-  submitForm(form: NgForm){
-    console.log(form.value);
-    form.reset();
+  submitForm(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+    if (this.isLoginMode) {
+    //...
+    } else {
+      //uses the method in the service to send sign up captured from the form into the database. Logs the data sent and error messages
+      this.authService.signUp(email, password).subscribe(
+        resData => {
+          console.log(resData);
+        },
+        error => {
+          console.log(error);
+        }
+      );
+      //resets the form afterwards
+      form.reset();
+    }
   }
-  ngOnInit(): void {
-  }
+
 
 }
