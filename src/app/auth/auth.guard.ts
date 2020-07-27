b729696d7700a1f960838dfa21b186ac53618d48
nateBannisZ <bannis52@gmail.com>
2020-07-27 18:41:37 -0400

    fixed bug where the subscription would occur forever

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
 import { Observable } from 'rxjs';
 import {AuthService} from './auth.service';
-import {map} from 'rxjs/operators';
+import {map, take} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +14,7 @@ export class AuthGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this.authService.user.pipe(map(user => {
+    return this.authService.user.pipe(take(1),map(user => {
       const isAuth = !!user;
       if(isAuth){
         return true;
